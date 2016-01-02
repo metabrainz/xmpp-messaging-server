2388870af14cccd968eda51295c0e2ca6ef579ac
Roman Tsukanov <roman@tsukanov.me>
2016-01-02 17:17:32 +0100

    Add docstrings for some function in ejabberd authentication script.

--- ejabberd_auth.py
+++ ejabberd_auth.py
@@ -13,6 +13,19 @@ import sys
 
 
 def from_ejabberd():
+    """Reads request from ejabberd.
+
+    Reads from stdin: AABBBBBBBBB.....
+
+    A: 2 bytes of length data (a short in network byte order)
+    B: a string of length found in A that contains operation in plain text operation are as follows:
+        auth:User:Server:Password (check if a username/password pair is correct)
+        isuser:User:Server (check if it's a valid user)
+        setpass:User:Server:Password (set user's password)
+        tryregister:User:Server:Password (try to register an account)
+        removeuser:User:Server (remove this account)
+        removeuser3:User:Server:Password (remove this account if the password is correct)
+    """
     input_length = sys.stdin.read(2)
     if len(input_length) != 2:
         return
@@ -21,6 +34,12 @@ def from_ejabberd():
 
 
 def to_ejabberd(success):
+    """Sends result to ejabberd.
+
+    AABB
+    A: the number 2 (coded as a short, which is bytes length of following result)
+    B: the result code (coded as a short), should be 1 for success/valid, or 0 for failure/invalid
+    """
     if success:
         answer = 1
     else:
